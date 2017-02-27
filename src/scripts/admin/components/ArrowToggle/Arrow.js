d07a2a3062e4d240cca9065fbedea43126273ed7
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2017-02-27 19:05:00 +0100

    allow multiline entities up to 10K lines 🖖

--- src/scripts/admin/components/ArrowToggle/Arrow.js
+++ src/scripts/admin/components/ArrowToggle/Arrow.js
@@ -18,7 +18,7 @@ import styled from 'styled-components';
 const Arrow = styled.div`
 	display: block;
 	position: absolute;
-	top: calc(50% - 8px)
+	top: 8px;
 	width: 8px;
 	height: 8px;
 	border-top: 8px solid transparent;
--- src/scripts/admin/components/EntityTile/Label.js
+++ src/scripts/admin/components/EntityTile/Label.js
@@ -16,13 +16,14 @@ const Label = styled.div`
 	display: inline-block;
 	position: relative;
 	box-sizing: border-box;
-	width: 200px;
+	max-width: 180px;
 	margin-top: 4px;
 	min-height: 16px;
 	line-height: 16px;
 	font-weight: 600;
 	font-size: 12px;
 	user-select: none;
+	hyphens: auto;
 	color: ${ props => 0 < props.entity.occurrences.length ? '#2e92ff' : '#666' };
 `;
 
